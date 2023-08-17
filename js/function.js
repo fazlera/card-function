ac49ba8ff799e9304fac3037f873a1d7a4257a18
FAZLE RABBI <mdfazlerabbim621@gmail.com>
2023-08-17 16:21:10 +0600

    updated

--- js/function.js
+++ js/function.js
@@ -1,11 +1,21 @@
+let priceTotal = 0;
+
 function getId(target){
     
     const cardTitle = target.parentNode.childNodes[1].innerText
     const li = document.createElement('li');
     li.innerText = cardTitle;
     const showProductName = document.getElementById('show-product-name');
-    showProductName.appendChild(li).style.color='red'
+    showProductName.appendChild(li).style.color='green'
 
-    const price = target.parentNode.childNodes[3].innerText;
-    console.log(price);
+    
+    const price = target.parentNode.childNodes[3].innerText.split(' ')[1];
+    
+     priceTotal = parseFloat(priceTotal) + parseFloat(price);
+    
+    document.getElementById('price-total').innerText = priceTotal;
+    
+     
+    
+
 }
