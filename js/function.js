fb7a7d5ee39c430b7f686c9d4f29f720aad0800a
FAZLE RABBI <mdfazlerabbim621@gmail.com>
2023-08-17 16:34:42 +0600

    set price

--- js/function.js
+++ js/function.js
@@ -3,17 +3,22 @@ let priceTotal = 0;
 function getId(target){
     
     const cardTitle = target.parentNode.childNodes[1].innerText
+
     const li = document.createElement('li');
     li.innerText = cardTitle;
+
     const showProductName = document.getElementById('show-product-name');
-    showProductName.appendChild(li).style.color='green'
+    showProductName.appendChild(li).style.color='green';
 
     
     const price = target.parentNode.childNodes[3].innerText.split(' ')[1];
-    
+
      priceTotal = parseFloat(priceTotal) + parseFloat(price);
     
-    document.getElementById('price-total').innerText = priceTotal;
+
+   document.getElementById('price-total').innerText = priceTotal;
+   
+
     
      
     
